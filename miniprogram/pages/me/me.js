672dfeb185164b69d3e9652b94cfb02379539dc2
Tom <56183524+thomashaku@users.noreply.github.com>
2020-07-12 03:25:44 +1000

    show function still developing
    <view bindTap> (already done), wx.showToast

--- miniprogram/pages/me/me.js
+++ miniprogram/pages/me/me.js
@@ -14,6 +14,20 @@ Page({
     })
   },
 
+  onTapAddress() {
+    wx.showToast({
+      icon: 'none',
+      title: 'This function is not open yet.'
+    })
+  },
+
+  onTapService() {
+    wx.showToast({
+      icon: 'none',
+      title: 'This function is not open yet.'
+    })
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
